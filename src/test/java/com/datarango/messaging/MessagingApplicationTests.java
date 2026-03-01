806a46164ed9ec4db30467a3f00905f3e8a25a87
Samson Okunola <smsnmicheal@gmail.com>
2026-03-01 11:43:20 +0100

    test: add TestPropertySource configuration for MessagingApplicationTests

--- src/test/java/com/datarango/messaging/MessagingApplicationTests.java
+++ src/test/java/com/datarango/messaging/MessagingApplicationTests.java
@@ -3,8 +3,17 @@ package com.datarango.messaging;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {
+	"eureka.client.enabled=false",
+	"spring.cloud.discovery.enabled=false",
+	"spring.cloud.loadbalancer.enabled=false",
+	"management.health.redis.enabled=false",
+	"azure.storage.connection-string=DefaultEndpointsProtocol=https;AccountName=test;AccountKey=dGVzdA==;EndpointSuffix=core.windows.net",
+	"azure.storage.container-name=test"
+})
 class MessagingApplicationTests {
 
 	@Test
@@ -12,3 +21,4 @@ class MessagingApplicationTests {
 	}
 
 }
+
