9681582151ba61f7bfdb064cb83d88d70a404e01
Samson Okunola <smsnmicheal@gmail.com>
2026-03-01 11:18:21 +0100

    test: update MessagingApplicationTests to use RANDOM_PORT for web environment config: add application.yaml for test configurations and service properties

--- src/test/java/com/datarango/messaging/MessagingApplicationTests.java
+++ src/test/java/com/datarango/messaging/MessagingApplicationTests.java
@@ -2,8 +2,9 @@ package com.datarango.messaging;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class MessagingApplicationTests {
 
 	@Test
